a0201a7f2529039bdc23812e85da1b9b5ab7f10f
offcwho <nhnhnhbgbg@gmail.com>
2026-06-10 18:30:04 +0300

    update

--- src/entities/cart/module/cart.data.ts
+++ src/entities/cart/module/cart.data.ts
@@ -16,5 +16,41 @@ export const INITIAL_ITEMS = [
         price: 1500,
         quantity: 1,
         image: 'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=300'
-    }
+    },
+    {
+        id: '3',
+        name: 'The Dark Lilac Chair',
+        category: 'Shown Edition',
+        specs: 'Fabric / Shearling Ivory / Coffee',
+        price: 1500,
+        quantity: 1,
+        image: 'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=300'
+    },
+    {
+        id: '4',
+        name: 'The Dark Lilac Chair',
+        category: 'Shown Edition',
+        specs: 'Fabric / Shearling Ivory / Coffee',
+        price: 1500,
+        quantity: 1,
+        image: 'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=300'
+    },
+    {
+        id: '5',
+        name: 'The Dark Lilac Chair',
+        category: 'Shown Edition',
+        specs: 'Fabric / Shearling Ivory / Coffee',
+        price: 1500,
+        quantity: 1,
+        image: 'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=300'
+    },
+    {
+        id: '6',
+        name: 'The Dark Lilac Chair',
+        category: 'Shown Edition',
+        specs: 'Fabric / Shearling Ivory / Coffee',
+        price: 1500,
+        quantity: 1,
+        image: 'https://images.unsplash.com/photo-1592078615290-033ee584e267?q=80&w=300'
+    },
 ];
