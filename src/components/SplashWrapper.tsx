621c95764d09e0ddb3cc2c514d10de06ad99d33d
offcwho <nhnhnhbgbg@gmail.com>
2026-06-16 03:26:34 +0300

    fix

--- src/components/SplashWrapper.tsx
+++ src/components/SplashWrapper.tsx
@@ -12,19 +12,16 @@ export default function SplashWrapper({ children }: { children: React.ReactNode 
         const isAppLoaded = sessionStorage.getItem('luxf_light_loaded');
 
         if (isAppLoaded) {
-            // Если это повторный переход — никакого сплэша, показываем сайт сразу
             setIsVisible(false);
         } else {
-            // 1. Даем нашей SVG-вспышке полностью отыграть (2 секунды)
             const fadeTimer = setTimeout(() => {
-                setIsFading(true); // Включаем fade-out эффект (opacity: 0)
+                setIsFading(true);
                 sessionStorage.setItem('luxf_light_loaded', 'true');
             }, 2000);
 
-            // 2. Ждем еще 500мс, пока завершится анимация растворения, и полностью удаляем сплэш
             const unmountTimer = setTimeout(() => {
                 setIsVisible(false);
-            }, 2500); // 2000мс заставка + 500мс плавный переход
+            }, 2500);
 
             return () => {
                 clearTimeout(fadeTimer);
@@ -43,9 +40,6 @@ export default function SplashWrapper({ children }: { children: React.ReactNode 
                 className={`fixed inset-0 z-99999 bg-white flex items-center justify-center transition-opacity duration-500 ease-in-out ${isFading ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
             >
                 <SplashScreen />
-            </div>
-            <div className="w-full h-full">
-                {children}
             </div>
         </>
     );
