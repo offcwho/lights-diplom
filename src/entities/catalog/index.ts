409661c95842e1d4e5c393c288e67f492fd7bbde
offcwho <nhnhnhbgbg@gmail.com>
2026-06-22 01:27:35 +0300

    fix

--- src/entities/catalog/index.ts
+++ src/entities/catalog/index.ts
@@ -1,7 +1,6 @@
 //Components
 export { CatalogCategoriesComponent as CatalogCategories } from "./components/catalog-categories.component";
 export { CatalogColorsComponent as CatalogColors } from "./components/catalog-colors.component";
-export { CatalogAttributesComponent as CatalogAttributes } from "./components/catalog-attributes.component";
 export { CatalogRangeComponent as CatalogRange } from "./components/catalog-range.component";
 export { CatalogSelectComponent as CatalogSelect } from "./components/catalog-select.component";
 
