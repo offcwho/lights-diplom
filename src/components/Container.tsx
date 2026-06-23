fc9dd5f607a9ccd0f0a9516a3ff07ee255066db1
offcwho <nhnhnhbgbg@gmail.com>
2026-06-23 10:42:04 +0300

    update

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Container: React.FC<Props> = ({ children, className }) => {
     return (
         <section
-            className={`max-w-7xl mx-auto space-y-16 lg:py-24 md:py-16 sm:py-12 xs:py-3 lg:px-6 md:px-5 sm:px-4 xs:px-2 ${className}`}
+            className={`max-w-7xl mx-auto space-y-16 lg:py-10 md:py-5 sm:py-2 xs:py-3 lg:px-6 md:px-5 sm:px-4 xs:px-2 ${className}`}
         >
             {children}
         </section>
